3d0f07191e57ebcb639c8a7553c613f988231f33
Nazar Aziz <nazar@clevertech.biz>
2014-10-08 10:00:00 +0100

    fix: ByIds getter fails if one single track causes a 500 error during the API call. Add some resiliency by attempting to import a batch tracks individually, skipping the offending track

--- src/services/soundcloud/SoundCloudTracksByIdsGetter.js
+++ src/services/soundcloud/SoundCloudTracksByIdsGetter.js
@@ -1,5 +1,6 @@
 var _ = require( 'lodash' );
 var rest = require( 'restler' );
+var Promise = require( 'bluebird' );
 
 //TODO add a logger - don't use console.log
 
@@ -32,16 +33,39 @@ exports = module.exports = function( config, promiser ){
 
         this.total = 0;
         this.errors = 0;
+
+        this.responses = [];
     }
 
 
     SoundCloudTracksByIdsGetter.prototype.execute = function(){
         return promiser( function( d ){
+            //attempt to get all tracks in one call...
+            //if this fails (which it might), get all batch tracks individually, skipping the bad track
+
+            getAllTracks.call( this )
+                .then( d.resolve )
+                .catch( function() {
+                    //batch import failed.... try individually
+                    getIndividually.call( this )
+                        .then( d.resolve )
+                        .catch( d.reject );
+
+                }.bind( this ) );
+
+        }, this );
+    };
+
+    ///////////////////
+    // private
+
+    function getAllTracks() {
+        return promiser( function( d ){
             var url;
 
             url = [
                 this.baseUrl,
-                'ids=' + this.ids.join(','),
+                    'ids=' + this.ids.join(','),
                 'limit=200'
             ].join( '&' );
 
@@ -53,10 +77,60 @@ exports = module.exports = function( config, promiser ){
                     d.resolve( data );
                 }.bind( this ) )
                 .on( 'fail', function( data, response ){
+                    console.log( 'Failed on batch import for url:', url);
+                    console.log( 'Importing tracks individually' );
                     d.reject( data, response );
                 }.bind( this ) );
         }, this );
-    };
+    }
+
+
+    function getIndividually() {
+        return Promise.cast( this.ids )
+            .each( function( soundcloudId ){
+
+                return getSoundcloudTrack.call( this, soundcloudId )
+                    .then( addToResponses.bind( this ) );
+
+            }.bind( this ) )
+            .then( returnResults.bind( this ) );
+    }
+
+    function returnResults() {
+        return _.flatten( this.responses );
+    }
+
+    function getSoundcloudTrack( soundcloudId ) {
+        return promiser( function( d ){
+            var url;
+
+            url = [
+                this.baseUrl,
+                'ids=' + soundcloudId
+            ].join( '&' );
+
+            //todo replace with a logger
+            console.log( 'Single SoundCloudTracksByIdsGetter url:', url );
+
+            rest.get( url )
+                .on( 'complete', function( data ){
+                    d.resolve( data );
+                }.bind( this ) )
+                .on( 'fail', function( ){
+                    console.log( 'Failed importing soundtrack id:', soundcloudId, '. Skipping');
+                    //silently ignore
+                    d.resolve();
+                }.bind( this ) );
+        }, this );
+    }
+
+    function addToResponses( response ) {
+        if ( response ) {
+            this.responses.push( response );
+        }
+    }
+
+    ////////////////////
 
     return SoundCloudTracksByIdsGetter;
 
